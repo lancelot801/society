276a38a6cee93572e4cf6fd6f0f8bdb1bc568ba8
lancelot801 <lancelot801@163.com>
2017-02-28 18:44:30 +0800

    add part of tools class

---
+++ src/main/java/cn/joongky/society/AbstractSociety.java
@@ -0,0 +1,8 @@
+package cn.joongky.society;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AbstractSociety implements Society{
+	public Logger logger = LoggerFactory.getLogger(this.getClass());
+}
