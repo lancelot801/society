eef7eb34c964bba9904b5dbafc73e8c3f0e024c5
lancelot801 <lancelot801@163.com>
2017-03-09 21:26:48 +0800

    修改society_apply表中的字段,去掉证件id,添加类别id

--- src/main/java/cn/joongky/society/dao/ActivityApplyMapper.java
+++ src/main/java/cn/joongky/society/dao/ActivityApplyMapper.java
@@ -7,27 +7,27 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.RowBounds;
 
 public interface ActivityApplyMapper {
-	int countByExample(ActivityApplyExample example);
+    int countByExample(ActivityApplyExample example);
 
-	int deleteByExample(ActivityApplyExample example);
+    int deleteByExample(ActivityApplyExample example);
 
-	int deleteByPrimaryKey(String activityId);
+    int deleteByPrimaryKey(String activityId);
 
-	int insert(ActivityApply record);
+    int insert(ActivityApply record);
 
-	int insertSelective(ActivityApply record);
+    int insertSelective(ActivityApply record);
 
-	List<ActivityApply> selectByExampleWithRowbounds(ActivityApplyExample example, RowBounds rowBounds);
+    List<ActivityApply> selectByExampleWithRowbounds(ActivityApplyExample example, RowBounds rowBounds);
 
-	List<ActivityApply> selectByExample(ActivityApplyExample example);
+    List<ActivityApply> selectByExample(ActivityApplyExample example);
 
-	ActivityApply selectByPrimaryKey(String activityId);
+    ActivityApply selectByPrimaryKey(String activityId);
 
-	int updateByExampleSelective(@Param("record") ActivityApply record, @Param("example") ActivityApplyExample example);
+    int updateByExampleSelective(@Param("record") ActivityApply record, @Param("example") ActivityApplyExample example);
 
-	int updateByExample(@Param("record") ActivityApply record, @Param("example") ActivityApplyExample example);
+    int updateByExample(@Param("record") ActivityApply record, @Param("example") ActivityApplyExample example);
 
-	int updateByPrimaryKeySelective(ActivityApply record);
+    int updateByPrimaryKeySelective(ActivityApply record);
 
-	int updateByPrimaryKey(ActivityApply record);
+    int updateByPrimaryKey(ActivityApply record);
 }
